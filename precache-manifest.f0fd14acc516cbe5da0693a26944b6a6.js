41ce248bdaf09ed332f8926b2fb0e814095a39b8
denis-md <dmalashin@gmail.com>
2020-05-21 20:13:53 +0300

    Updates

---
+++ precache-manifest.f0fd14acc516cbe5da0693a26944b6a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6ea944ef5c33b4caca3b705e74b93f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df403ce8784b5bbf861",
+    "url": "/static/css/main.1b0c66a8.chunk.css"
+  },
+  {
+    "revision": "e99767152551f7e5fbff",
+    "url": "/static/js/2.a2d355c3.chunk.js"
+  },
+  {
+    "revision": "a1a88636b752ef668fe9699df3c2d360",
+    "url": "/static/js/2.a2d355c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df403ce8784b5bbf861",
+    "url": "/static/js/main.73b0c766.chunk.js"
+  },
+  {
+    "revision": "51b883e44e4e3293bf39",
+    "url": "/static/js/runtime-main.832203ed.js"
+  }
+]);
